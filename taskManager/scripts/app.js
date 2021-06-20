57ec0e7c7d8bc9a227ed01abb0611d3b924144ac
Leo <83550516+Leopoldo-Flores@users.noreply.github.com>
2021-06-20 00:09:20 -0700

    Update app.js

--- taskManager/scripts/app.js
+++ taskManager/scripts/app.js
@@ -1,21 +1,21 @@
 var important = false;
 var serverUrl = "http://fsdiapi.azurewebsites.net/";
 
-function toggleImpotant(){
+function toggleImportant(){
     if(!important){
-        important=true;
-        $("#Important").removeClass("far").addClass("fas");
-    }
-    else{
-        important=false;
-        $("#Important").removeClass("far").addClass("fas");
-    }
+        important = true;
+        $("#iImportant").removeClass("far").addClass("fas");
+     }else{
+         important= false;
+         $("#iImportant").removeClass("fas").addClass("far");
+     }
 }
 
 function saveTask(){
+    console.log("save");
     let title = $("#txtTitle").val();
+    let description = $("#txtDescription").val();
     let category = $("#selCategory").val();
-    let description = $("#txtDesctiption").val();
     let location = $("#txtLocation").val();
     let dueDate = $("#selDueDate").val();
     let color = $("#selColor").val();
@@ -26,15 +26,17 @@ function saveTask(){
 
     //send object to a backend server
     $.ajax({
-        url: serverUrl + "api/tasks/",
-        type: 'POST',
+        url: serverUrl +"api/tasks/",
+        type:"POST",
         data: JSON.stringify(task),
-        contentType: "application/json",
-        success: function(res) {
-            console.log("Server says:", res);
+        contentType:"application/json",
+        success: function(res){
+            console.log("SERVER ", res);
+
         },
-        error: function(eDetails){ 
-            console.error("Error saving", eDetails);
+        error: function(eDetails){
+            console.error("ERROR Saving", eDetails);
+
         }
     });
 
@@ -49,7 +51,7 @@ function displayTask(task){
         <i class='important fas fa-star'></i> 
         <div class="description">
             <h5>${task.title}</h5>
-            <p>${task.desctiprion}</p>
+            <p>${task.description}</p>
         </div>
         <lable class="due-date">${task.dueDate}</lable>
         <lable class="location">${task.location}</lable>
@@ -63,9 +65,8 @@ function init(){
     console.log("My Task Manager");
 
     //load data
-
     //hook events
-    $("#iImportant").click(toggleImpotant);
+    $("#iImportant").click(toggleImportant);
     $("#btnSave").click(saveTask);
 }
 
